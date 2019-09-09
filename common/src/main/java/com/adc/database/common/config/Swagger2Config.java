2b0f7a39aa7450f52763987c9f46ec09a8298d95
song <sw326142308@126.com>
2019-09-09 14:32:40 +0800

    第一次提交

--- common/src/main/java/com/adc/database/common/config/Swagger2Config.java
+++ common/src/main/java/com/adc/database/common/config/Swagger2Config.java
@@ -17,18 +17,18 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
-    @Bean
-    public Docket webApiConfig(){
-
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("webApi")
-                .apiInfo(webApiInfo())
-                .select()
-                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
-                .paths(Predicates.not(PathSelectors.regex("/error.*")))
-                .build();
-
-    }
+//    @Bean
+//    public Docket webApiConfig(){
+//
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .groupName("webApi")
+//                .apiInfo(webApiInfo())
+//                .select()
+//                .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
+//                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+//                .build();
+//
+//    }
 
     @Bean
     public Docket adminApiConfig(){
@@ -42,20 +42,20 @@ public class Swagger2Config {
 
     }
 
-    private ApiInfo webApiInfo(){
-
-        return new ApiInfoBuilder()
-                .title("网站-课程中心API文档")
-                .description("本文档描述了课程中心微服务接口定义")
-                .version("1.0")
-                .build();
-    }
+//    private ApiInfo webApiInfo(){
+////
+////        return new ApiInfoBuilder()
+////                .title("网站-课程中心API文档")
+////                .description("本文档描述了课程中心微服务接口定义")
+////                .version("1.0")
+////                .build();
+////    }
 
     private ApiInfo adminApiInfo(){
 
         return new ApiInfoBuilder()
-                .title("后台管理系统-课程中心API文档")
-                .description("本文档描述了后台管理系统课程中心微服务接口定义")
+                .title("数据库管理系统")
+                .description("本文档描述了后台管理系统课程中心接口定义")
                 .version("1.0")
                 .build();
     }
